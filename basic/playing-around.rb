6a51fcdf3fde1189666456c3360f516cfac8408d
JeffreyDinackus <jeffreydinackus@proton.me>
2023-11-27 09:35:35 -0500

    playing around

---
+++ basic/playing-around.rb
@@ -0,0 +1,17 @@
+
+# Define an array of jokes
+jokes = [
+  "Why don't scientists trust atoms? Because they make up everything!",
+  "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them!",
+  "Why don't skeletons fight each other? They don't have the guts!",
+  "Why don't eggs tell jokes? Because they might crack up!",
+  "Why did the scarecrow win an award? Because he was outstanding in his field!"
+]
+
+# adds a extra jokes
+jokes << "I like pie!"
+
+
+# Print a random joke from the array
+
+puts jokes.sample
